c747c1c163184a4628c0084677aa136e952f850c
octomwm <valentinschmid@neomailbox.ch>
2014-01-24 15:29:39 +0100

    icon theme mod

--- scripts/cpgithub.sh
+++ scripts/cpgithub.sh
@@ -10,6 +10,7 @@ cp -r ~/.config/awesome -t ~/Documents/Github/dotfiles
 
 # save gtk theme and icons to dotfiles/theme
 cp -r ~/.themes/Phosbird -t ~/Documents/Github/dotfiles/theme
+cp -r ~/.icons/Phosphore -t ~/Documents/Github/dotfiles/theme
 
 # save config files to dotfiles/config
 cp ~/.vimrc ~/.gvimrc ~/.zshrc ~/.gmrunrc ~/.Xdefaults -t ~/Documents/Github/dotfiles/config
@@ -21,8 +22,9 @@ cp -r ~/.fonts/PragmataPro -t ~/Documents/Github/dotfiles/config/.fonts
 cp ~/.config/clipit/clipitrc -t ~/Documents/Github/dotfiles/config/.config/clipit
 cp ~/.config/zathura/zathurarc -t ~/Documents/Github/dotfiles/config/.config/zathura
 cp -r ~/.config/ranger -t ~/Documents/Github/dotfiles/config/.config
-sudo cp -r ~/.vim -t ~/Documents/Github/dotfiles/config/
 cp ~/.config/Thunar/uca.xml -t ~/Documents/Github/dotfiles/config/.config/Thunar
+
+cp -r ~/.vim/colors -t ~/Documents/Github/dotfiles/config/.vim
 
 # save firefox related files to dotfiles/userstyle
 cp -r ~/Apps/newweathertab/ -t ~/Documents/Github/dotfiles/userstyles
